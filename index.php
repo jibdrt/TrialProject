704b9f9a1eb2833828f0e5f1a6f2d0c03712c0db
JbDruet <jb.druet@codeur.online>
2021-08-16 22:15:20 +0200

    scrollListeners

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Parallax</title>
+</head>
+<body>
+<section>
+    <img src="images/ls1.png" alt="" id="base1">
+    <img src="images/ls1.png" alt="" class="base1">
+
+
+    <h2 id="text">PORTFOLIO</h2>
+</section>
+<script src="main.js"></script>
+</body>
+</html>
+
---
+++ main.js
@@ -0,0 +1,22 @@
+let base1 = document.getElementById('base1');
+let base2 = document.getElementById('base2');
+let base3 = document.getElementById('base3');
+let base4 = document.getElementById('base4');
+let text = document.getElementById('text');
+
+window.addEventListener('scroll', function(){
+    var value = window.scrollY;
+    base1.style.top = -value*0.5+'px';
+    base2.style.top = value*0.8+'px';
+    base3.style.top = value*1+'px';
+    base4.style.top = value*1+'px';
+    text.style.top = value*0.5+'px';
+});
+
+window.addEventListener('scroll', function(){
+    var value = window.pageYOffset;
+    text.classList.add('smooth');
+    text.style.marginRight = value * (-1);
+    console.log(value);
+    
+});
